d37d550b8365c564063c1a8f8d4e4cb19b4a9ff5
Jeremy Meadows <jeremybmeadows@gmail.com>
2020-10-07 14:58:18 -0500

    creates persistant login cookies to store a current session

---
+++ food-truck-frontend/src/pages/auth/logout.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from '@material-ui/core/Link';
+import {useRouter} from "next/router";
+import sha256 from 'js-sha256';
+import NavMenu from "../navmenu";
+import user from '../utils/user';
+import { useCookies, Cookies } from 'react-cookie';
+import HomePage from '../homepage';
+
+require('dotenv').config();
+
+function Logout() {
+    const [cookies, setCookie, removeCookie] = useCookies(['sessionUser']);
+    removeCookie('sessionUser');
+    
+    return (
+        <div>
+            <NavMenu></NavMenu>
+            <div style={{textAlign: 'center', marginTop: '10vh'}}>
+            <h1>You have successfully logged out.</h1>
+            <a href='/'>return to the home page</a>
+            </div>
+        </div>
+    )
+}
+
+export default Logout
--- food-truck-frontend/src/pages/dashboard.js
+++ food-truck-frontend/src/pages/dashboard.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import Link from '@material-ui/core/Link';
 import {useRouter} from "next/router";
+import { useHistory } from 'react-router-dom';
 import NavMenu from "./navmenu";
+import user from './utils/user.js';
+import { useCookies } from 'react-cookie';
 
 require('dotenv').config();
 
 function Dashboard() {
     const router = useRouter();
+    const [cookies, setCookie] = useCookies(['sessionUser']);
+
+
+    console.log(cookies.sessionUser);
+    if (cookies.sessionUser === undefined) {
+        console.log('redirecting');
+    }
+
     return (
         <div>
             <NavMenu></NavMenu>
-            <h2>This is the dashboard!</h2>
+            <h2 style={{textAlign: 'center'}}>This is { cookies.sessionUser }'s dashboard!</h2>
         </div>
     )
 }
+
+
 
 export default Dashboard
