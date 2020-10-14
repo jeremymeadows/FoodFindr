6dcbf63fdcc8284a3df84844be8a64f01e99ff54
Jeremy Meadows <jeremybmeadows@gmail.com>
2020-10-13 19:32:47 -0500

    somehow I deleted a '}' during the merge

--- food-truck-frontend/src/pages/navmenu.js
+++ food-truck-frontend/src/pages/navmenu.js
@@ -22,13 +22,12 @@ function NavMenu() {
                 marginBottom: 8, width: '181px', height: '48px'}}>Dashboard</a></Link>
                 <Link href={"/trucks"}><a style={{marginLeft: 8, marginRight: 8, marginTop: 8,
                     marginBottom: 8, width: '181px', height: '48px'}}>Trucks</a></Link>
-            </span>
+            </span>}
             {sess && <span style={{float: 'right', textAlign: 'right', margin: '20px'}}>
             <Link href={"/auth/login"}><a style={{marginLeft: 8, marginRight: 8, marginTop: 8,
                 marginBottom: 8, width: '181px', height: '48px'}}>Login</a></Link>
             <Link href={"/auth/register"}><a style={{marginLeft: 8, marginRight: 8, marginTop: 8,
             marginBottom: 8, width: '181px', height: '48px'}}>Create Account</a></Link>
-
             </span>}
             {!sess && <span style={{float: 'right', textAlign: 'right', margin: '20px'}}>
                 <Link href={"/manageaccount"}><a style={{marginLeft: 8, marginRight: 8, marginTop: 8,
