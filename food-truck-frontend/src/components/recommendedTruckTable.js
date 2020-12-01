42dbaafa3b3e49022cc5c79aa0d1597fcd3e90cd
kt <kjwokoek@gmail.com>
2020-11-30 22:58:58 -0600

    initial recommended alg working

--- food-truck-frontend/src/components/recommendedTruckTable.js
+++ food-truck-frontend/src/components/recommendedTruckTable.js
@@ -36,6 +36,16 @@ class TruckTable extends Component {
             });
         console.log("OUT");
 
+        console.log("getting subscriptions");
+        let b = this.getSubscriptions().then(() => {
+            this.forceUpdate();
+        });
+        let result = await b;
+
+        /*for (let v = 0; v < this.state.subs.length; v++) {
+            console.log("SSSSSS: " + this.state.sub[v]);
+        }*/
+
         let fetchData = {method: 'post', body: this.state.user.id};
 
         console.log("getting preferences");
@@ -72,45 +82,125 @@ class TruckTable extends Component {
             }).catch(error => console.log(error));
 
         console.log("Rec algorithm ");
+        let multipliers = [];
         let temptrucks = [];
+        let alltrucks = []
         let cost = this.state.preferences[0].price;
         let rate = this.state.preferences[0].rating;
         let type = this.state.preferences[0].type;
+        let iter = 0;
         console.log("PRICE: " + this.state.preferences[0].price);
         if(this.state.preferences) {
-            console.log("ALG");
+            console.log("Price Mult");
             this.state.trucks.forEach(function (truck) {
                 console.log("cost inside for: " + cost);
                 console.log("TRUCK PRICE: " + truck.price + " RATING: " + truck.rating);
-                if (cost === truck.price && rate === truck.rating) {
+                let multiplier = 4;
+                let constval = 3;
+                if (cost === truck.price) {
                     console.log("ONE");
-                    temptrucks.unshift(truck);
-                } else if (cost === truck.price) {
-                    console.log("TWO");
-                    temptrucks.push(truck);
+                    constval *= multiplier;
+                    multipliers.push(constval);
+                } else {
+                    multiplier = 3;
+                    if (truck.price === 3) {
+                        constval = 1;
+                        constval *= multiplier;
+                        multipliers.push(constval);
+                    }
+                    else if (truck.price === 2) {
+                        constval = 2;
+                        constval *= multiplier;
+                        multipliers.push(constval);
+                    }
+                    else {
+                        constval = 3;
+                        constval *= multiplier;
+                        multipliers.push(constval);
+                    }
                 }
-                /*else if (this.state.preferences.includes(truck.rating)) {
-                    temptrucks.push(truck);
-                }*/
             });
+            console.log("MUL: " + multipliers);
+            this.state.trucks.forEach(function (truck) {
+                console.log("rating inside for: " + rate);
+                console.log("RATING: " + truck.rating);
+                let multiplier = 3;
+                let constval = 5;
+                if (rate === truck.rating) {
+                    console.log("ONE");
+                    constval *= multiplier;
+                    multipliers[iter] += constval;
+                } else {
+                    multiplier = 2;
+                    if (truck.rating === 5) {
+                        constval *= multiplier;
+                        multipliers[iter] += constval;
+                    } else if (truck.rating === 4) {
+                        constval = 4;
+                        constval *= multiplier;
+                        multipliers[iter] += constval;
+                    } else if (truck.rating === 3) {
+                        constval = 3;
+                        constval *= multiplier;
+                        multipliers[iter] += constval;
+                    } else if (truck.rating === 2) {
+                        constval = 2;
+                        constval *= multiplier;
+                        multipliers[iter] += constval;
+                    } else {
+                        constval = 1;
+                        constval *= multiplier;
+                        multipliers[iter] += constval;
+                    }
+                }
+                iter++;
+            });
+            iter = 0;
+            let subscriptions = [];
+            for (let i = 0; i < this.state.subs.length; i++) {
+                console.log("S: " + this.state.subs);
+                subscriptions.push(this.state.subs[i]);
+            }
+            this.state.trucks.forEach(function (truck) {
+                console.log("SUBS: " + subscriptions);
+                if (subscriptions.includes(truck.id)) {
+                    console.log("Subscribed");
+                    multipliers[iter] += 10;
+                }
+                iter++;
+            });
+            iter = 0;
             this.state.trucks.forEach(function (truck) {
                 if (!temptrucks.includes(truck)) {
                     temptrucks.push(truck);
                 }
             });
+            while (iter < this.state.trucks.length) {
+                let max = multipliers[0];
+                let pos = 0;
+                for (let i = 0; i < temptrucks.length; i++) {
+                    if (multipliers[i] > max) {
+                        max = multipliers[i];
+                        pos = i;
+                    }
+                }
+                alltrucks.push(temptrucks[pos]);
+                multipliers[pos] = -1;
+                iter++;
+            }
         } else {
             this.state.trucks.forEach(function (truck) {
-                temptrucks.push(truck);
-
+                alltrucks.push(truck);
             });
         }
-        this.setState({trucks: temptrucks});
+        this.setState({trucks: alltrucks});
         this.forceUpdate();
 
 
     }
 
     async getSubscriptions() {
+        console.log("Went to get subs");
         if (this.state.user !== null) {
             await fetch('http://localhost:8080/user/' + this.state.user.id)
                 .then(res => res.json())
