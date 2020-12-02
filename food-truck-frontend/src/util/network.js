ab602f9010e91a00847f6c4d4261383ffe5c2d59
Jeremy Meadows <jeremybmeadows@gmail.com>
2020-12-02 15:31:02 -0600

    cleanup

--- food-truck-frontend/src/util/network.js
+++
@@ -1,4 +0,0 @@
-const host = 'http://localhost:8080/';
-// const host = 'http://test.jmeadows.xyz:8080/';
-
-export default host;
---
+++ frontend/src/util/network.js
@@ -0,0 +1,4 @@
+// const host = 'http://localhost:8080/';
+const host = 'http://foodfindr.jmeadows.xyz:8080/';
+
+export default host;
